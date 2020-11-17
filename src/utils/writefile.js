570b3c875250deafda80519cc11c5028c3247644
Jack McNicol <jack.mcnicol@aligent.com.au>
2020-11-17 19:50:11 +1030

    add eslint

--- src/utils/writefile.js
+++ src/utils/writefile.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 
 export const writeFile = (file, data) => new Promise(
-    (resolve, reject) => {
-        fs.writeFile(file, data, 'utf8', (err) => {
-            if (err) {
-                return reject(err)
-            }
-            resolve(data)
-        });
-    }
-)
+  (resolve, reject) => {
+    fs.writeFile(file, data, 'utf8', (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  },
+);
--- src/writer.js
+++ src/writer.js
@@ -1,14 +1,12 @@
-import { writeFile } from './utils/writefile';
 import path from 'path';
-import { parseAsync }  from 'json2csv';
-
+import { parseAsync } from 'json2csv';
+import { writeFile } from './utils/writefile';
 
 export const writeEstimateToDisk = async (data) => {
-    const fileJSON = path.resolve(process.cwd(), 'output/estimates.json');
-    const fileCSV = path.resolve(process.cwd(), 'output/estimates.csv');
-    const csvData = await parseAsync(data)
-    await writeFile(fileJSON, JSON.stringify(data));
-    await writeFile(fileCSV, csvData);
-    console.log('Successfully wrote data');
-
-}
+  const fileJSON = path.resolve(process.cwd(), 'output/estimates.json');
+  const fileCSV = path.resolve(process.cwd(), 'output/estimates.csv');
+  const csvData = await parseAsync(data);
+  await writeFile(fileJSON, JSON.stringify(data));
+  await writeFile(fileCSV, csvData);
+  console.log('Successfully wrote data');
+};
